f4c1d0396735b5d7ab5e88978889666ca31b13c3
LaszloLajosT <track.runner800@outlook.com>
2020-10-11 18:17:21 +0100

    Migrate AndroidX

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,11 +18,12 @@ package com.example.android.android_me.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
@@ -91,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         intent.putExtras(b);
 
         // The "Next" button launches a new AndroidMeActivity
-        Button nextButton = (Button) findViewById(R.id.next_button);
+        Button nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
